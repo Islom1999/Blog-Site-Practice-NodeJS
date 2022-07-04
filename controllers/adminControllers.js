11dfd48fd92c21aaf16b3547b0de98b619e867f2
Islom1999 <islomkarimovalisherovich@gmail.com>
2022-07-04 15:01:05 +0500

    admin page  blog post add, delete and update finish

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -27,6 +27,50 @@ const getBlogPage = async(req,res) => {
     })
 }
 
+const getOneBlogPage = async(req,res) => {
+    const BlogDataDB = await BlogPost.findById(req.params.id).lean()
+
+    res.render('admin/adminOneBlog', {
+        admin: 'admin',
+        url: process.env.URL,
+        BlogPostDB: BlogDataDB, 
+        pageBlog: 'blog'
+    })
+}
+
+const putOneBlogPage = async(req,res) => {
+    const BlogDataDB = await BlogPost.findById(req.params.id).lean()
+
+    const {title, descr} = req.body
+
+    let NewBlogDataDB = {
+        title: title ? title : BlogDataDB.title,
+        descr: descr ? descr : BlogDataDB.descr,
+        image: Boolean(req.file) ? 'img/blog/' + req.file.filename : BlogDataDB.image
+    }
+
+    await BlogPost.findByIdAndUpdate(req.params.id, NewBlogDataDB)
+
+    res.redirect(`/admin/blog/${BlogDataDB._id}`)            
+}
+
+const deleteOneBlogPage = async(req,res) => {
+    await BlogPost.findByIdAndDelete(req.params.id)
+    res.redirect('/admin/blog')
+}
+
+const postBlogPage = async(req,res) => {
+    const {title, descr} = req.body
+
+    await BlogPost.create({
+        title,
+        descr,
+        image:'img/blog/' + req.file.filename
+    })
+    
+    res.redirect('/admin/blog')  
+}
+
 const getContactPage = async(req,res) => {
     res.render('admin/adminContact', {
         admin: 'admin',
@@ -48,7 +92,7 @@ const getAboutPage = async(req,res) => {
         url: process.env.URL,
         pageAbout: 'about'
     })
-}
+}  
 
 module.exports = {
     getAdminPage,
@@ -56,5 +100,9 @@ module.exports = {
     getBlogPage,
     getContactPage,
     getPortfolioPage,
-    getAboutPage
+    getAboutPage,
+    postBlogPage,
+    getOneBlogPage,
+    putOneBlogPage,
+    deleteOneBlogPage
 }
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,17 @@
 const { Router } = require('express')
+const upload = require('../utils/fileUploads.js')
+
 const {
     getAdminPage,
     getHomePage,
     getBlogPage,
     getContactPage,
     getPortfolioPage,
-    getAboutPage
+    getAboutPage,
+    postBlogPage,
+    getOneBlogPage,
+    putOneBlogPage,
+    deleteOneBlogPage
 } = require('../controllers/adminControllers')
 
 const router = Router()   
@@ -13,7 +19,14 @@ const router = Router()
 // "/" => "url + /admin"
 router.get('/', getAdminPage)
 router.get('/home', getHomePage)
+
 router.get('/blog', getBlogPage)
+router.post('/blog', upload.single('image'), postBlogPage)
+
+router.get('/blog/:id', getOneBlogPage)
+router.post('/blog/:id', upload.single('image'), putOneBlogPage)
+router.post('/blog/:id/delete', deleteOneBlogPage)
+
 router.get('/contact', getContactPage)
 router.get('/portfolio', getPortfolioPage)
 router.get('/about', getAboutPage)
